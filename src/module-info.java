03ee7746c0fe98896d99202d3fc7a6cf8afca1e1
TMP21 <pierre.franke@aim.com>
2025-01-21 13:04:44 +0100

    Update

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-
-module CocktailBar {
-    requires java.datatransfer;
-    requires java.desktop;
-    requires javafx.graphics;
-    requires javafx.controls;
-    opens util to javafx.graphics;
-}
